9014248aaeabb2e39421585376409efb55bd140d
Jaarabytes <xavierandole78@gmail.com>
2024-07-03 16:33:18 +0300

    feat: add simple homepage
    Add homepage where users see that we are in devlopment mode, thanks to
    all.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import '../app.css'
+  
+  // add donations as soon as possible
+  const builders = [{name: "Jaarabytes", link: "jaarabytes"}]
+</script>
+
+<div class='my-5 px-3 sm:px-[20%]'>
+  <p class='text-3xl sm:text-[49px] font-extrabold'>Learn DevOps, the fun way! </p>
+    <div class='my-5'>
+      <p class=''>Unfortunately we are still in development. </p>
+      <p>We however accept all contributions (ideas, code itself and donations) </p>
+      <p class='mt-5'>Builders: </p>
+      {#each builders as builder}
+        <li><a href={`https://x.com/${builder.link}`}>{builder.name}</a></li>
+      {/each}
+      <p class="my-5">You can contact us through X (formerly twitter) DM's. </p>
+      <p class=''>For code contributions: </p>
+      <a href="https://github.com/Jaarabytes/OpsArena">https://github.com/Jaarabytes/OpsArena</a>
+      <p class='my-3'>Issues: </p>
+      <a href="https://github.com/Jaarabytes/OpsArena/issues/new">https://github.com/Jaarabytes/OpsArena/issues/new</a>
+    </div>
+</div>
