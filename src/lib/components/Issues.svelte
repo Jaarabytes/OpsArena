761f24820c6bfe68ad3f3ae20c31eaac7fd67092
Jaarabytes <xavierandole78@gmail.com>
2024-07-06 22:39:51 +0300

    feat: issues component
    To be pasted everywhere since there will be issues

---
+++ src/lib/components/Issues.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "../../app.css"
+</script>
+
+<main class='my-5 sm:px-[20%] px-3'>
+  <p class='text-2xl'>If you have an issues on the code and want to contribute: </p>
+    <a class='text-xl font-bold' href='https://github.com/jaarabytes/ops-arena/issues/new'>https://github.com/jaarabytes/ops-arena</a>  
+    <p class= 'text-2xl mt-5'>If you have an issues on the content, use this link: </p>
+    <a class='text-xl font-bold' href='https://github.com/jaarabytes/ops-arena/issues/new'>https://github.com/jaarabytes/ops-arena/issues/new</a>  
+</main>
