c86c1c34b2bd9631e7dcb4dbb1507182133c76e9
Jaarabytes <xavierandole78@gmail.com>
2024-07-06 22:39:23 +0300

    feat: simple footer
    Will fix it later

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<footer class='inline flex justify-center'>
+  <a target="_blank" rel="noreferrer" href={`/privacy`} class="mx-5 my-3">Privacy</a>
+  <a target="_blank" rel="noreferrer" href="https://twitter.com/jaarabytes" class="mx-5 my-3">Contact</a>
+  <a target="_blank" rel="noreferrer" href={`/terms`}  class="my-3 mx-5">Terms of service</a>
+</footer>
