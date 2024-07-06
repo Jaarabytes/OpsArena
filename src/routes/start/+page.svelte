0214c70b0bc7126cd02e96f06ddf66cc235342f0
Jaarabytes <xavierandole78@gmail.com>
2024-07-06 22:40:17 +0300

    feat: start page
    Users can maybe learn now. Still starting

---
+++ src/routes/start/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Courses from "$lib/components/Courses.svelte";
+  import "../../app.css"
+  import Issues from "$lib/components/Issues.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+</script>
+<Courses />
+<Issues />
+<Footer />
