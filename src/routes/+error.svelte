6cb4ec3ed9270befd78e0749ef5c4f86df3d8057
Jaarabytes <xavierandole78@gmail.com>
2024-07-16 20:26:53 +0300

    http cat
    No explanation

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,29 +1,27 @@
 <script>
   import { page } from '$app/stores'
   import { onMount } from 'svelte'
-  // Please use http status dogs
   import '../app.css'
 
-  // haha, already using it 
 
-  let dogImage = '';
+  let catImage = '';
   
   // okay, this is exactly useEffect
   onMount(() => {
     if ( $page.status === 404 || $page.status === 500 ) {
       // only for 404, 500 and 401 maybe
-      dogImage = `https://http.dog./${$page.status}.jpg`
+      catImage = `https://http.cat./${$page.status}.jpg`
     }
   })
 </script>
 
 <svelte:head>
-  <title>{$page.status}: {$page.error.message}</title>
+  <title>{$page.status}: {$page.error?.message}</title>
 </svelte:head>
 
 <main>
-  {#if dogImage }
-    <a href="/"><img src={dogImage} class='mx-auto' alt="HTTP status dog!" /></a>
+  {#if catImage }
+    <a href="/"><img src={catImage} class='mx-auto' alt="HTTP status dog!" /></a>
   {:else}
     <p class='text-center my-5'>An error occured. Please go <a href="/">HOME</a></p>
   {/if}
