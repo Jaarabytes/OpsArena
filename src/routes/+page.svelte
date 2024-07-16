8655b682c27136bed04c2dce19b7e6414be2f49f
Jaarabytes <xavierandole78@gmail.com>
2024-07-16 20:26:43 +0300

    refactor: better homepage
    Homepage is better now, more efficient. To add a better styling here

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,23 @@
 <script>
   import '../app.css'
-  
-  // add donations as soon as possible
-  const builders = [{name: "Jaarabytes", link: "jaarabytes"}]
+  const links = [{name: "Github, ", href: "https://github.com/jaarabytes/ops-arena"},
+  {name: "Twitter, ", href: "https://x.com/jaarabytes"},
+  {name: "and Status page.", href: "/donate"}]
 </script>
 
 <svelte:head>
   <title>OpsArena</title>
 </svelte:head>
 
-<div class='my-5 px-3 sm:px-[20%]'>
-  <p class='text-3xl sm:text-[49px] font-extrabold'>Learn DevOps, the fun way! </p>
-    <div class='my-5'>
-      <p class=''>Unfortunately we are still in development. </p>
-      <p>We however accept all contributions (ideas, code itself and donations) </p>
-      <p class='mt-5'>Builders: </p>
-      {#each builders as builder}
-        <li><a href={`https://x.com/${builder.link}`}>{builder.name}</a></li>
-      {/each}
-      <p class="my-5">You can contact us through X (formerly twitter) DM's. </p>
-      <p class=''>For code contributions: </p>
-      <a href="https://github.com/Jaarabytes/OpsArena">https://github.com/Jaarabytes/OpsArena</a>
-      <p class='my-3'>Issues: </p>
-      <a href="https://github.com/Jaarabytes/OpsArena/issues/new">https://github.com/Jaarabytes/OpsArena/issues/new</a>
-      <p class='my-3'>To donate: </p>
-      <a href='/donate'>Click me!</a>
+<main class="flex items-center justify-center h-screen">
+  <div class='mx-auto px-3 xl:px-[40%]'>
+    <p class='text-3xl my-5 sm:text-[49px] font-extrabold'>Ops-arena</p>
+    <p>Learn DevOps, the <b>fun</b> way! </p>
+    <button class="text-white border border-slate-700 w-full py-3 my-5 rounded-lg text-xl hover:bg-yellow-900">Login</button>
+    <div class="my-5 ">
+      {#each links as link}
+      <a class="text-white hover:text-yellow-700" href={link.href}>{link.name}</a>
+    {/each}
     </div>
-</div>
+  </div>
+</main>
