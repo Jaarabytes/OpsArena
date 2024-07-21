30c40d2c7c137f08f5c3b7c201f3e50356eea0ff
jaarabytes <xavierandole78@gmail.com>
2024-07-21 09:01:34 +0300

    feat: add more status codes
    Add 403 and 401 to the error page.
    Also, fixed the alt message to show http-status-cat instead of
    http-status-dog

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,13 +3,12 @@
   import { onMount } from 'svelte'
   import '../app.css'
 
-
   let catImage = '';
   
   // okay, this is exactly useEffect
   onMount(() => {
-    if ( $page.status === 404 || $page.status === 500 ) {
-      // only for 404, 500 and 401 maybe
+    if ( $page.status === 404 || $page.status === 500 || $page.status === 403 || $page.status === 401 ) {
+      // showcase a cat image for each http status error! Hell yeah
       catImage = `https://http.cat./${$page.status}.jpg`
     }
   })
@@ -21,7 +20,7 @@
 
 <main>
   {#if catImage }
-    <a href="/"><img src={catImage} class='mx-auto' alt="HTTP status dog!" /></a>
+    <a href="/"><img src={catImage} class='mx-auto' alt="HTTP status cat!" /></a>
   {:else}
     <p class='text-center my-5'>An error occured. Please go <a href="/">HOME</a></p>
   {/if}
